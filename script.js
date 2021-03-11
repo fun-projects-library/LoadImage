37fbfedc2d347ead31057fd105389ebf08f30f62
fehmi-enes-eren <76635233+fehmi-enes-eren@users.noreply.github.com>
2021-03-10 21:13:35 -0700

    createFolder

---
+++ script.js
@@ -0,0 +1,63 @@
+
+const imgContainer = document.querySelector(".images");
+
+
+// "./images/img-1.jpg"
+
+
+
+
+const createImage = function (imgPath) {
+
+    return new Promise( (resolve, reject) => {
+        
+            let imgTag = document.createElement("img");
+            imgTag.src = imgPath;
+            console.log(imgTag);
+            imgContainer.appendChild(imgTag);
+
+            imgTag.addEventListener("load", function(){
+                console.log(`Image is loaded!!!`);
+                resolve(imgTag);
+            });
+            imgTag.addEventListener("error" , function(){
+                reject(`We are unable to load the image at this moment!!!`);
+            });
+    })
+  
+}
+
+
+let currentImage;
+const waitFor = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+createImage("./images/img-1.jpg")
+    .then( (result)=> {
+        currentImage = result; 
+        return waitFor(2000)})
+    .then( ()=> {
+        currentImage.style.display = "none";
+        return createImage("./images/img-2.jpg")})
+    .then( (result)=> {
+        currentImage = result; 
+        return waitFor(2000)})
+    .then( ()=> {
+        currentImage.style.display = "none";
+        return createImage("./images/img-3.jpg")})
+    .catch( (err)=> {
+        console.log(err)
+    })
+
+
+
+
+//call createImage with then and catch
+
+
+
+// create a wait function
+// const waitFor = function (second) {
+
+    
+
+// }
